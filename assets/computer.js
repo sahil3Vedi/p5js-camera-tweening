4e05d8c84c154184f22147fb20094a130109caa4
sahil3vedi <sahiltrivediw@gmail.com>
2023-07-31 21:10:27 +0530

    Using classes now

--- assets/computer.js
+++ assets/computer.js
@@ -1,27 +1,22 @@
-function displayComputer(color1, color2){
-    // Computer is rendered at 0,0,0
-    push();
-    var [cx,cy,cz] = color1;
-    directionalLight(cx,cy,cz,lfx,lfy,lfz);
-    ambientLight(cx,cy,cz);
-    ambientMaterial(cx,cy,cz);
-    // main frame
-    box(400,225,30);
-    // stand
-    translate(0,137,0);
-    box(50,50,20);
-    // base
-    translate(0,32,0);
-    box(100,15,50);
-    // keyboard
-    var [cx,cy,cz] = color2;
-    directionalLight(cx,cy,cz,lfx,lfy,lfz);
-    ambientLight(cx,cy,cz);
-    ambientMaterial(cx,cy,cz);
-    translate(0,0,125);
-    box(250,15,75);
-    // mouse
-    translate(160,3,0);
-    box(25,9,40);
-    pop();
+class Computer {
+    constructor(color1,color2,scale,position){
+        this.color1 = color(color1);
+        this.color2 = color(color2);
+        this.scale = scale;
+        this.position = position;
+    }
+
+    show(){
+        push();
+        translate(this.position[0],this.position[1],this.position[2]);
+
+        // screen
+        push();
+        specularMaterial(this.color1);
+        shininess(50);
+        box(960*this.scale, 540*this.scale,200*this.scale);
+        pop();
+
+        pop();
+    }
 }
--- scene.js
+++ scene.js
@@ -1,25 +1,21 @@
-const lightSourceFront = [0,2400,-2400];
-const lightSourceBack = [0,-2400,-2400];
-const [lfx,lfy,lfz] = lightSourceFront;
-const [lbx,lby,lbz] = lightSourceBack;
+let computer;
 
 function setup(){
     var canvas = createCanvas(window.innerWidth, window.innerHeight, WEBGL);
     canvas.parent('sketch3D');
     createEasyCam();
     document.oncontextmenu = () => false;
+    computer = new Computer("#0f172a","#94a3b8",1,[0,0,0]);
 }
 
-function loadModels(){
+function showModels(){
     strokeWeight(0);
-    displayComputer([161,161,161],[30,41,59]);
-    displayTable([161,98,7]);
-    displayWallsAndFloor([255,251,235],[255,237,213]);
-    displayBed([161,98,7],[186,230,253],[113,63,18],[133,77,14])
+    computer.show();
 }
 
 function draw(){
     background(226,232,240);
     drawAxes(400);
-    loadModels();
+    pointLight(255,255,255, -1000, -1000, 1000);
+    showModels();
 }
